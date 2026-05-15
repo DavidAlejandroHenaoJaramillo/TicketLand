ee7e462132bd99d9f6f86b09222dff7825af8ac1
DavidAlejandroHenaoJaramillo <davida.henaoj@uqvirtual.edu.co>
2026-05-14 23:54:31 -0500

    Cambios en clase usuario

--- src/main/java/com/example/ticketland/Usuario.java
+++ src/main/java/com/example/ticketland/Usuario.java
@@ -1,4 +1,11 @@
 package com.example.ticketland;
 
 public class Usuario {
+    private String id;
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private String metodosPago;
+    private String historialCompras;
+
 }
